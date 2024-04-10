75bfde3070b76902263e7e92acadd1c67996b9c3
Kathleen <kbacigalupi@smith.edu>
2024-04-10 08:49:41 -0400

    xkcd function

--- R/xkcd.R
+++ R/xkcd.R
@@ -6,10 +6,27 @@
 #' describing that comic from the official xkcd API.
 #' @importFrom jsonlite read_json
 #' @export
-
 xkcd <- function(number) {
   url <- file.path("https://xkcd.com", number, "info.0.json")
   results <- jsonlite::read_json(url)
-  return(results)
+  x <- new_xkcd(results)
+  x <- validate_xkcd(x)
+  return(x)
 }
 
+new_xkcd <- function(x) {
+
+  stopifnot(is.list(x))
+
+  structure(x,
+            class = "skcd")
+}
+
+validate_xkcd <- function(x) {
+  required_fields <- c("month", "num", "link", "year", "news", "safe_title",
+                       "transcript", "alt", "img", "title", "day")
+
+  if(!all(required_fields %in% names(x))) {
+    stop ("xkcd object is missing some fields")
+  }
+}
